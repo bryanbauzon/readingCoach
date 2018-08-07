c94c482309162afca5d44c5076e5093dd48255ce
Timothy <timothyjhey@gmail.com>
2018-08-07 18:39:26 +0800

    Poem Level done.

---
+++ app/src/main/java/sebastian/devmonkey/capstoneproject/activity/Poem/PoemDifficulty.java
@@ -0,0 +1,45 @@
+package sebastian.devmonkey.capstoneproject.activity.Poem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import sebastian.devmonkey.capstoneproject.R;
+
+public class PoemDifficulty extends AppCompatActivity {
+
+    ArrayAdapter<String> listview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_poem_difficulty);
+
+
+        // array
+        String[] menuItems = {"Story 1", "Story 2", "Story 3"};
+
+        ListView listView1 = findViewById(R.id.easyList);
+        ListView listView2 = findViewById(R.id.mediumList);
+        ListView listView3 = findViewById(R.id.hardList);
+
+        //get array and display to listview
+        listview = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                menuItems
+        );
+
+
+        listView1.setAdapter(listview);
+        listView2.setAdapter(listview);
+        listView3.setAdapter(listview);
+
+
+
+
+
+
+    }
+}
