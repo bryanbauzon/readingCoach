8262ace1bb1e920815a7fd3b4435f7a387cf0e5a
Timothy <timothyjhey@gmail.com>
2018-08-09 17:17:19 +0800

    Added sample story in poem. Doesn't have text to speech yet.

---
+++ app/src/main/java/sebastian/devmonkey/capstoneproject/activity/Poem/Story.java
@@ -0,0 +1,15 @@
+package sebastian.devmonkey.capstoneproject.activity.Poem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import sebastian.devmonkey.capstoneproject.R;
+
+public class Story extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story);
+    }
+}
